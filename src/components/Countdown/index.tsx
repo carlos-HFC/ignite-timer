ccfee7d2dd9a45aeae70f1bc4860d6dba1ea01c5
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-18 13:12:23 -0300

    feat: add components

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,64 @@
+import { differenceInSeconds } from "date-fns"
+import { useEffect } from "react"
+
+import { useCycles } from "../../contexts/Cycle"
+
+import { CountdownContainer, Separator } from "./style"
+
+export function Countdown() {
+  const {
+    activeCycle,
+    activeCycleId,
+    amountSecondsPassed,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+  } = useCycles()
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, "0")
+  const seconds = String(secondsAmount).padStart(2, "0")
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setSecondsPassed(totalSeconds)
+          return clearInterval(interval)
+        }
+
+        setSecondsPassed(secondsDifference)
+      }, 1000)
+    }
+
+    return () => clearInterval(interval)
+  }, [
+    activeCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    setSecondsPassed,
+    totalSeconds,
+  ])
+
+  return (
+    <CountdownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownContainer>
+  )
+}
---
+++ src/components/NewCycleForm/index.tsx
@@ -0,0 +1,43 @@
+import { useFormContext } from "react-hook-form"
+
+import { useCycles } from "../../contexts/Cycle"
+
+import { FormContainer, MinutesAmountInput, TaskInput } from "./style"
+
+export function NewCycleForm() {
+  const { activeCycle } = useCycles()
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        {...register("task")}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Banana" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        id="minutesAmount"
+        type="number"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
